3dd79013def83a22e96414cc54ac4b2444f5cbd2
Jon Cho <jonathan@Jons-MacBook-Pro.local>
2024-11-30 22:10:27 +0000

    remove popup2

--- extension/popup_2.js
+++
@@ -1,8 +0,0 @@
-// popup.js
-document.getElementById("start-selection").addEventListener("click", async () => {
-    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-    chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        files: ["selection.js"]
-    });
-});
